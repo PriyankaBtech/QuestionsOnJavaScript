71ef579808bbd218c0f0a7c643860604658e285e
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-09-06 23:56:56 +0530

    solved questions related to error handling and prototypes

---
+++ 02_ErrorHandling_&_Prototypes/3_ques.js
@@ -0,0 +1,22 @@
+// 3. Car Description Class.
+
+// Create a class called Car with three properties: company, model, and year.
+// The class should have a method called getDescription that returns a string in the format "This is a <year> <company> <model>".
+// Instantiate an instance of the Car class and call the getDescription method.
+
+
+class Car {
+    constructor(company, model, year) {
+      this.company = company;
+      this.model = model;
+      this.year = year;
+    }
+    getDescription() {
+        return `This is a ${this.year} ${this.company} ${this.model}.`;
+    }
+}
+    
+const myCar = new Car("New Delhi", "India", 2023);
+
+console.log(myCar.getDescription()); // This is a 2023 New Delhi India.
+
---
+++ 02_ErrorHandling_&_Prototypes/4_ques.js
@@ -0,0 +1,27 @@
+/*
+4. Employee Class Challenge.
+
+Create a class called Employee with three properties: name, position, and salary.
+The class should have a method called getSalary that returns the employee's salary.
+Instantiate an instance of the Employee class and call the getSalary method.
+*/
+
+class Employee {
+    constructor(name, position, salary) {
+      this.name = name;
+      this.position = position;
+      this.salary = salary;
+    }
+  
+    getSalary() {
+      return this.salary;
+    }
+}
+
+const employee1 = new Employee("Priyanka", "Software Engineer", 80000);
+
+// Output
+console.log(employee1.getSalary()); // 80000
+
+  
+
